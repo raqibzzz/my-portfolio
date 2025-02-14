dd764ab02bafbc92aac8bd9fe0e1706f3f60154d
raqibzzz <raqibmuktadir2@gmail.com>
2025-02-13 19:27:36 -0500

    newpage.tsx

---
+++ src/app/newpage.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import HorizontalLayout from '@/components/HorizontalLayout';
+
+export default function Home() {
+  return (
+    <main>
+      <HorizontalLayout />
+    </main>
+  );
+}
